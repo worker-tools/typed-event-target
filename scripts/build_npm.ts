bd1eda42bbc181f20ba11152f7d36dda3c4deb6c
Florian Klampfer <mail@qwtel.com>
2024-07-24 09:58:00 +0700

    Commit

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env -S deno run --allow-read --allow-write=./,/Users/qwtel/Library/Caches/deno --allow-net --allow-env=HOME,DENO_AUTH_TOKENS,DENO_DIR --allow-run=git,pnpm
 
-import { basename } from "https://deno.land/std@0.133.0/path/mod.ts";
-import { build, emptyDir } from "https://deno.land/x/dnt@0.23.0/mod.ts";
+import { basename } from "jsr:@std/path";
+import { build, emptyDir } from "jsr:@deno/dnt@0.41.2";
 
 import { 
   copyMdFiles, mkPackage,
-} from 'https://gist.githubusercontent.com/qwtel/ecf0c3ba7069a127b3d144afc06952f5/raw/latest-version.ts'
+} from 'https://gist.githubusercontent.com/qwtel/ecf0c3ba7069a127b3d144afc06952f5/raw/e6499586c53ee3c78e01ea2bc3a9f16610129e96/latest-version.ts'
 
 await emptyDir("./npm");
 
@@ -17,11 +17,12 @@ await build({
   shims: {},
   test: false,
   package: await mkPackage(name),
-  declaration: true,
+  declaration: "inline",
   packageManager: 'pnpm',
   compilerOptions: {
     sourceMap: true,
-    target: 'ES2019'
+    target: 'ES2019',
+    lib: ['ES2019', 'DOM']
   },
 });
 
